93f6b19baeea070e58f14e6fd2e020d094a1b848
Pedro Sánchez <mrchunckuee.psr@gmail.com>
2024-11-18 00:25:03 -0600

    Uso de dos puertos seriales: U0UXD y U2UXD

---
+++ ESP32-DevKitv1_UART_to_UART/ESP32-DevKitv1_UART_to_UART.ino
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ *
+ *          ESP32: Uso de puertos seriales
+ *
+ *******************************************************************************
+ * FileName:        ESP32-DevKitv1_UART_to_UART.ino
+ * Processor:       ESP32
+ * Complier:        Arduino IDE v2.3.3
+ * Author:          Pedro Sánchez (MrChunckuee)
+ * Blog:            http://mrchunckuee.blogspot.com/
+ * Email:           mrchunckuee.psr@gmail.com
+ * Description:     Pequeño ejemplo para testear el manejo del puerto serial 2 
+ *                  que incorpora el ESP32-WROOM. Como sabemos este incroporta 
+ *                  tres puertos seriales por hardware: U0UXD, U1UXD y U2UXD.
+ *                     - U0UXD, se usa principalmente para programar el modulo, 
+ *                              pero tambien puede ser de proposito general.
+ *                     - U1UXD, se usa para comunicar con una memoria SPI flash 
+ *                              interna del ESP32. No recomendable para el usar.
+ *                     - U2UXD, libre para proposito general.
+ *******************************************************************************
+ * Rev.         Date            Comment
+ *   v1.0.0     17/11/2024      - Creación y prueba de funcionamiento 
+ ******************************************************************************/
+#define RXD2 16
+#define TXD2 17
+
+void setup() {
+  Serial.begin(115200);
+  //Formato configurar serial 1: Serial1.begin(baud-rate, protocol, RX pin, TX pin);
+  Serial1.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
+  Serial.println("U0UXD - Serial TXD is on pin: "+String(TX));
+  Serial.println("U0UXD - Serial RXD is on pin: "+String(RX));
+  
+  Serial1.println("U2UXD - Serial TXD is on pin: "+String(TXD2)); 
+  Serial1.println("U2UXD - Serial RXD is on pin: "+String(RXD2)); 
+}
+
+void loop() {
+  // Leemos puerto 2 y enviamos al purto 0:
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte);
+  }
+
+  // Leemos puerto 0 y enviamos al purto 2:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
+  }
+}
