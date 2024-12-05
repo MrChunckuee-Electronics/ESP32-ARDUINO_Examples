89283298b8a3d4ac07918f212ec9e77fe396be08
Pedro Sánchez <mrchunckuee.psr@gmail.com>
2024-12-05 00:02:42 -0600

    Uso de dos puertos seriales: U0UXD y U2UXD (v1.0.1)

--- ESP32-DevKitv1_UART_to_UART/ESP32-DevKitv1_UART_to_UART.ino
+++ ESP32-DevKitv1_UART_to_UART/ESP32-DevKitv1_UART_to_UART.ino
@@ -19,33 +19,35 @@
  *                     - U2UXD, libre para proposito general.
  *******************************************************************************
  * Rev.         Date            Comment
- *   v1.0.0     17/11/2024      - Creación y prueba de funcionamiento 
+ *   v1.0.0     17/11/2024      - Creación y prueba de funcionamiento.
+ *   v1.0.1     04/12/2024      - Correecion para asegurarnos el uso de U2UXD.
  ******************************************************************************/
+ 
 #define RXD2 16
 #define TXD2 17
 
 void setup() {
   Serial.begin(115200);
   //Formato configurar serial 1: Serial1.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial1.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 
   Serial.println("U0UXD - Serial TXD is on pin: "+String(TX));
   Serial.println("U0UXD - Serial RXD is on pin: "+String(RX));
   
-  Serial1.println("U2UXD - Serial TXD is on pin: "+String(TXD2)); 
-  Serial1.println("U2UXD - Serial RXD is on pin: "+String(RXD2)); 
+  Serial2.println("U2UXD - Serial TXD is on pin: "+String(TXD2)); 
+  Serial2.println("U2UXD - Serial RXD is on pin: "+String(RXD2)); 
 }
 
 void loop() {
-  // Leemos puerto 2 y enviamos al purto 0:
-  if (Serial1.available()) {
-    int inByte = Serial1.read();
+  // Leemos puerto 2 y enviamos al puerto 0:
+  if (Serial2.available()) {
+    int inByte = Serial2.read();
     Serial.write(inByte);
   }
 
-  // Leemos puerto 0 y enviamos al purto 2:
+  // Leemos puerto 0 y enviamos al puerto 2:
   if (Serial.available()) {
     int inByte = Serial.read();
-    Serial1.write(inByte);
+    Serial2.write(inByte);
   }
 }
